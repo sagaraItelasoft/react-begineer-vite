efa4f52be15521469bf469eef3f9e110750e3b19
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-09-08 22:51:13 +0530

    how use effect works

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import { ClassComponent } from "./ClassComponent"
+import { useState } from "react"
+import { Child } from "./Child"
 
-function App() {
-  return <ClassComponent />
+export default function App() {
+  const [show, setShow] = useState(true)
+
+  const childComponent = show ? <Child /> : null
+
+  return (
+    <div>
+      <button onClick={() => setShow(currentShow => !currentShow)}>
+        Show/Hide
+      </button>
+      {childComponent}
+    </div>
+  )
 }
-
-export default App
