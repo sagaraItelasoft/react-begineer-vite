2d51551e3125bf88926b61d780789694e29e94f4
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-09-08 21:15:06 +0530

    code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import './styles.css'
-import TodoList from './components/TodoList'
-import user from "./user.json"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <TodoList/>
-      <h1>Hi imported json user data file </h1>
-      <h3>{JSON.stringify(user)}</h3>
-      <h1>Hi imported json image </h1>
-      <img src={viteLogo} className="logo" alt="Vite logo" />
-
     </>
   )
 }
