dda061a5bf27b11121ff649b3bbdd9bced76b671
Sagara Dissanayake <sagara.dissanayake@itelasoft.com>
2023-09-08 23:07:41 +0530

    fecth users inside useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState } from "react"
-import { Child } from "./Child"
+import { useEffect, useState } from "react"
+import { User } from "./User"
 
-export default function App() {
-  const [show, setShow] = useState(true)
+function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [users, setUsers] = useState([])
 
-  const childComponent = show ? <Child /> : null
+  useEffect(() => {
+    setIsLoading(true)
+
+    const controller = new AbortController()
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      signal: controller.signal,
+    })
+      .then(res => res.json())
+      .then(setUsers)
+      .finally(() => {
+        setIsLoading(false)
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
 
   return (
-    <div>
-      <button onClick={() => setShow(currentShow => !currentShow)}>
-        Show/Hide
-      </button>
-      {childComponent}
-    </div>
+    <>
+      <h1>User List</h1>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <ul>
+          {users.map(user => {
+            return <User key={user.id} name={user.name} />
+          })}
+        </ul>
+      )}
+    </>
   )
 }
+
+export default App
